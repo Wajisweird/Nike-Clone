ec9c442e38f8adce068acee660ad4592c0c7cbe9
Qazi Wajahat Hussain <wajahathuss17@gmail.com>
2023-10-30 14:55:57 +0500

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,19 +8,20 @@ export default function About() {
         <br />
         <br />
         <div className="row  d-flex align-items-center mt-5" id="responsive">
-          <div className="col-6" data-aos="fade-right" data-aos-duration="4000" id="football">
+          <div className="col-6" data-aos="fade-right" id="football">
             <img src={footballer} alt="/" />
           </div>
-          <br /><br />
+          <br />
+          <br />
           <div className="col-6 text-center" id="ballText">
             <p className="austraila mt-5 text-white">Be legendary!</p>
             <br />
-            <p className="textera text-white-50" data-aos="fade-left" data-aos-duration="4000">
+            <p className="textera text-white-50">
               Impossible is just a big word thrown around by small men who find
               it easier to the world they’ve been given than to explore the
               power they have to change it. Impossible is not a fact. It’s an
-              opinion. Impossible is not a declaration. <br />It’s a dare. Impossible
-              is potential. Impossible is temporary.
+              opinion. Impossible is not a declaration. <br />
+              It’s a dare. Impossible is potential. Impossible is temporary.
             </p>
             <br />
           </div>
